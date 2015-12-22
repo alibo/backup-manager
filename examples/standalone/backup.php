52a9937757750313a703827fc7cf2192cf27f15e
Ali Borhani <aliborhani1@gmail.com>
2015-12-22 17:39:55 +0330

    add delete old files to examples

--- examples/standalone/backup.php
+++ examples/standalone/backup.php
@@ -2,3 +2,6 @@
 
 $manager = require 'bootstrap.php';
 $manager->makeBackup()->run('development', 's3', 'test/backup.sql', 'gzip');
+
+// delete old files
+$manager->deleteOldFiles()->run('s3','test/', new \DateTime('2016-01-01'));
